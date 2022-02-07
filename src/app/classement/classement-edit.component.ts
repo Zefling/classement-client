7f351589cd3e1eae81a3b5a11bd66307d3373d5e
Zéfling <zefling@ikilote.net>
2022-02-07 23:12:07 +0100

    Correction drag'n-drop

--- src/app/classement/classement-edit.component.ts
+++ src/app/classement/classement-edit.component.ts
@@ -10,11 +10,12 @@ type Group = { name: string; color: string; list: string[] };
 })
 export class ClassementEditComponent {
     groups: Group[] = [
-        { name: 'A', color: '#FFFFFF', list: [] },
-        { name: 'B', color: '#FFFFFF', list: [] },
-        { name: 'C', color: '#FFFFFF', list: [] },
-        { name: 'D', color: '#FFFFFF', list: [] },
-        { name: 'E', color: '#FFFFFF', list: [] },
+        { name: 'S', color: '#dc8add', list: [] },
+        { name: 'A', color: '#f66151', list: [] },
+        { name: 'B', color: '#ffbe6f', list: [] },
+        { name: 'C', color: '#f9f06b', list: [] },
+        { name: 'D', color: '#8ff0a4', list: [] },
+        { name: 'E', color: '#99c1f1', list: [] },
     ];
     list: string[] = [
         'Element 1',
@@ -27,17 +28,29 @@ export class ClassementEditComponent {
         'Element 8',
         'Element 9',
         'Element 10',
+        'Element 11',
+        'Element 12',
+        'Element 13',
+        'Element 14',
+        'Element 15',
+        'Element 16',
+        'Element 17',
+        'Element 18',
+        'Element 19',
+        'Element 20',
     ];
 
-    drop(event: CdkDragDrop<string[]>) {
-        if (event.previousContainer === event.container) {
-            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    drop(list: string[], event: CdkDragDrop<{ list: string[]; index: number }>) {
+        const indexFrom = event.previousContainer.data.index;
+        const indexTarget = event.container.data.index;
+        if (event.previousContainer.data.list === event.container.data.list) {
+            moveItemInArray(list, indexFrom, indexTarget);
         } else {
             transferArrayItem(
-                event.previousContainer.data,
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex,
+                event.previousContainer.data.list,
+                event.container.data.list,
+                indexFrom,
+                indexTarget + event.currentIndex,
             );
         }
     }
