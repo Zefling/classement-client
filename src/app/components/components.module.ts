0da01394e3e2c8847365296b64959dcefe694cb3
Zéfling <zefling@ikilote.net>
2022-02-14 21:09:54 +0100

    Fix dialog

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { DialogComponent } from './dialog.component';
 
 @NgModule({
     declarations: [DialogComponent],
     exports: [DialogComponent],
+    imports: [BrowserModule],
 })
 export class ComponentsModule {}
