7563156d38eb7b03e3ffdfb151afdc10e7936ad7
Zéfling <zefling@ikilote.net>
2022-02-20 01:29:20 +0100

    Détection auto de la langue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,26 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 
+const languages = [
+    { value: 'en', label: 'English' },
+    { value: 'fr', label: 'Français' },
+];
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    languages = languages;
     selectedLang = 'en';
 
     constructor(private _translate: TranslateService) {
+        // autodetect language
+        const l = languages.filter(i => navigator.language.startsWith(i.value));
+        if (l.length) {
+            this.selectedLang = l[0].value;
+        }
         this._translate.reloadLang(this.selectedLang);
     }
 
