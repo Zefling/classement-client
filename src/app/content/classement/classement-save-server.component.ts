28b7f68eb102e67e3d9f0fa6e1bd19cce81da318
Zéfling <zefling@ikilote.net>
2022-07-09 23:51:28 +0200

    Mise en forme du cropper

--- src/app/content/classement/classement-save-server.component.ts
+++ src/app/content/classement/classement-save-server.component.ts
@@ -5,6 +5,9 @@ import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
 import { DialogComponent } from 'src/app/components/dialog.component';
 import { Classement, FileString, FormatedGroup, Options } from 'src/app/interface';
 import { APIUserService } from 'src/app/services/api.user.service';
+import { OptimiseImageService } from 'src/app/services/optimise-image.service';
+
+import { categories } from './classement-default';
 
 
 @Component({
@@ -30,10 +33,12 @@ export class ClassementSaveServerComponent {
 
     update = false;
 
+    categories = categories;
+
     imageChangedEvent: any = '';
     croppedImage: any = '';
 
-    constructor(private userService: APIUserService) {
+    constructor(private userService: APIUserService, private optimiseImage: OptimiseImageService) {
         this.userService.loggedStatus().then(() => {
             if (this.userService.logged) {
                 if (this.userService.user!.username === this.classement?.user) {
@@ -56,15 +61,34 @@ export class ClassementSaveServerComponent {
     fileChangeEvent(event: any): void {
         this.imageChangedEvent = event;
     }
-    imageCropped(event: ImageCroppedEvent) {
-        this.croppedImage = event.base64;
+
+    async imageCropped(event: ImageCroppedEvent) {
+        if (this.classement && event.base64) {
+            const banner = await this.optimiseImage.resize(
+                {
+                    name: 'banner',
+                    url: event.base64,
+                    realSize: event.base64.length,
+                    date: new Date().getTime(),
+                    size: 0,
+                    type: 'image/png',
+                },
+                300,
+                300,
+            );
+            this.classement.banner = banner.reduceFile?.url || banner.sourceFile.url!;
+            this.croppedImage = this.classement.banner;
+        }
     }
+
     imageLoaded(image: LoadedImage) {
         // show cropper
     }
+
     cropperReady() {
         // cropper ready
     }
+
     loadImageFailed() {
         // show message
     }
