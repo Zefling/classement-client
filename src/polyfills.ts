6828637883fba6f5d5b9188816132963afe69bf8
Zéfling <zefling@ikilote.net>
2024-05-09 12:49:04 +0200

    Add HammerJS for NgxImageCropper

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -50,3 +50,5 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'hammerjs';
