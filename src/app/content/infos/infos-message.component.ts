d16380a5be78e90fd8a7d9bac9db1e2966344cc0
Zéfling <zefling@ikilote.net>
2025-02-09 01:37:18 +0100

    Rewrite forms with Magma: login & infos message

--- src/app/content/infos/infos-message.component.ts
+++ src/app/content/infos/infos-message.component.ts
@@ -1,22 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { FormBuilderExtended, MagmaInput, MagmaInputElement, MagmaInputText, MagmaInputTextarea } from '@ikilote/magma';
 import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 
 import { MessageService, MessageType } from 'src/app/components/info-messages/info-messages.component';
 import { APIUserService } from 'src/app/services/api.user.service';
 import { GlobalService } from 'src/app/services/global.service';
 import { Subscriptions } from 'src/app/tools/subscriptions';
-import { Utils } from 'src/app/tools/utils';
-
-import { TextareaAutosizeDirective } from '../../directives/textarea-autosize.directive';
 
 @Component({
     selector: 'infos-message',
     templateUrl: './infos-message.component.html',
     styleUrls: ['./infos-message.component.scss'],
-    imports: [FormsModule, TextareaAutosizeDirective, TranslocoPipe],
+    imports: [ReactiveFormsModule, TranslocoPipe, MagmaInput, MagmaInputElement, MagmaInputText, MagmaInputTextarea],
 })
 export class InfosMessageComponent {
     private readonly userService = inject(APIUserService);
@@ -24,19 +22,18 @@ export class InfosMessageComponent {
     private readonly translate = inject(TranslocoService);
     private readonly global = inject(GlobalService);
     private readonly router = inject(Router);
-
-    text = '';
-    username = '';
-    email = '';
+    private readonly fbe = inject(FormBuilderExtended);
 
     private _sub = Subscriptions.instance();
 
+    readonly formConfig: FormGroup<{
+        username: FormControl<string>;
+        email: FormControl<string>;
+        message: FormControl<string>;
+    }>;
+
     constructor() {
         this.updateTitle();
-
-        if (this.userService.user) {
-            this.username = this.userService.user.username;
-        }
 
         this._sub.push(
             this.translate.langChanges$.subscribe(() => {
@@ -47,22 +44,49 @@ export class InfosMessageComponent {
         if (!this.global.withApi()) {
             this.router.navigate(['/infos/licenses']);
         }
+
+        const base = 'user.information.contact.admin.error.';
+
+        this.formConfig = this.fbe.groupWithErrorNonNullable({
+            username: {
+                default: this.userService.user?.username ?? '',
+                control: {
+                    required: { state: true, message: this.translate.translate(base + 'username.required') },
+                },
+            },
+            email: {
+                default: '',
+                control: {
+                    required: { state: true, message: this.translate.translate(base + 'email.required') },
+                    email: { message: this.translate.translate(base + 'email.email') },
+                },
+            },
+            message: {
+                default: '',
+                control: {
+                    required: { state: true, message: this.translate.translate(base + 'message.required') },
+                    minlength: { state: 20, message: this.translate.translate(base + 'message.minlength') },
+                    maxlength: { state: 5000, message: this.translate.translate(base + 'message.maxlength') },
+                },
+            },
+        });
     }
 
     updateTitle() {
         this.global.setTitle('menu.information');
     }
 
-    invalideMessage(): boolean {
-        return this.text.trim().length < 20 || !Utils.testEmail(this.email) || !this.username.trim().length;
-    }
-
     send() {
-        if (this.username.trim().length && this.email.trim().length && this.text.trim().length) {
+        this.formConfig.updateValueAndValidity({ onlySelf: true });
+        this.formConfig.markAllAsTouched();
+        if (this.formConfig.valid) {
             this.userService
-                .sendToAdmin(this.username, this.email, this.text)
+                .sendToAdmin(this.formConfig.value as { username: string; email: string; message: string })
                 .then(() => {
-                    this.text = '';
+                    const message = this.formConfig.get('message');
+                    message?.setValue('');
+                    message?.markAsUntouched();
+                    message?.markAsPristine();
                     this.messageService.addMessage(this.translate.translate('message.contact.sent.success'));
                 })
                 .catch(e => {
--- src/app/content/user/user-login.component.ts
+++ src/app/content/user/user-login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit, booleanAttribute, inject, input } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
+import { FormBuilderExtended, MagmaInput, MagmaInputElement, MagmaInputPassword, MagmaInputText } from '@ikilote/magma';
 import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
 
 import { APIUserService } from 'src/app/services/api.user.service';
@@ -15,16 +16,24 @@ import { LoaderComponent } from '../../components/loader/loader.component';
     selector: 'user-login',
     templateUrl: './user-login.component.html',
     styleUrls: ['./user-login.component.scss'],
-    imports: [FormsModule, RouterLink, LoaderComponent, TranslocoPipe],
+    imports: [
+        RouterLink,
+        LoaderComponent,
+        TranslocoPipe,
+        ReactiveFormsModule,
+        MagmaInput,
+        MagmaInputElement,
+        MagmaInputPassword,
+        MagmaInputText,
+    ],
 })
 export class UserLoginComponent implements OnInit, OnDestroy {
     private readonly router = inject(Router);
     private readonly userService = inject(APIUserService);
     private readonly translate = inject(TranslocoService);
     private readonly global = inject(GlobalService);
+    private readonly fbe = inject(FormBuilderExtended);
 
-    username = '';
-    password = '';
     showError = '';
 
     loader = false;
@@ -32,6 +41,29 @@ export class UserLoginComponent implements OnInit, OnDestroy {
     popup = input<boolean, any>(false, { transform: booleanAttribute });
 
     private listener = Subscriptions.instance();
+
+    readonly formLogin: FormGroup<{
+        username: FormControl<string>;
+        password: FormControl<string>;
+    }>;
+
+    constructor() {
+        this.formLogin = this.fbe.groupWithErrorNonNullable({
+            username: {
+                default: '',
+                control: {
+                    required: { state: true, message: this.translate.translate('user.login.username.required') },
+                },
+            },
+            password: {
+                default: '',
+                control: {
+                    minlength: { state: 10, message: this.translate.translate('user.login.password.minlength') },
+                    required: { state: true, message: this.translate.translate('user.login.password.required') },
+                },
+            },
+        });
+    }
 
     ngOnInit(): void {
         this.updateTitle();
@@ -58,21 +90,30 @@ export class UserLoginComponent implements OnInit, OnDestroy {
         this.listener.clear();
     }
 
-    submit() {
+    submit(event: MouseEvent) {
         this.loader = true;
-        this.userService
-            .login(this.username, this.password)
-            .then(() => {
-                if (!this.popup()) {
-                    this.router.navigate(['/user/profile']);
-                }
-            })
-            .catch(e => {
-                this.showError = e;
-            })
-            .finally(() => {
-                this.loader = false;
-            });
+        this.fbe.validateForm(this.formLogin);
+        console.log(this.formLogin.valid, this.formLogin.value.username, this.formLogin.value.password);
+        if (this.formLogin.valid && this.formLogin.value.username && this.formLogin.value.password) {
+            debugger;
+            this.userService
+                .login(this.formLogin.value.username, this.formLogin.value.password)
+                .then(() => {
+                    if (!this.popup()) {
+                        this.router.navigate(['/user/profile']);
+                    }
+                })
+                .catch(e => {
+                    this.showError = e;
+                })
+                .finally(() => {
+                    this.loader = false;
+                });
+        } else {
+            this.loader = false;
+        }
+        event.stopPropagation();
+        event.preventDefault();
     }
 
     oauth(service: string) {
